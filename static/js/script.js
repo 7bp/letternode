c550085092fdb4b96535236d40f3be4654efdccf
knalli <knallisworld@googlemail.com>
2013-01-03 18:55:43 +0100

    Improve demo-script.js to control the player's step & result.

--- static/js/script.js
+++ static/js/script.js
@@ -18,37 +18,69 @@ $(document).ready(function() {
 
   // Debugging
   (function() {
+    var stateMatrix = $('#stateMatrix'), convertMatrixArrayToPrint = function(input) {
+      var result = '';
+      for (var i = 0, c = input.length; i < c; i += 5) {
+        result += input.slice(i, i + 5).join(' ');
+        result += '\n';
+      }
+      return result;
+    }, convertMatrixStringToPrint = function(input) {
+      var result = '';
+      for (var i = 0, c = input.length; i < c; i += 5) {
+        result += input.substring(i, 5);
+        result += '\n';
+      }
+      return result;
+    };
     socket.on('gameCreated', function(data) {
-      game = data.game;
+      game = data.game || {};
       $('#gameId').val(game.player2);
       console.log(data);
       console.info('Game ID: ' + game.id + 'Player 2 ID: ' + game.player2);
     });
     socket.on('playerJoined', function(data) {
-      game = data.game;
+      game = data.game || {};
       console.log(data);
       console.info('Action: ' + 'playerJoined', game);
     });
     socket.on('playerLeft', function(data) {
-      game = data.game;
+      game = data.game || {};
       console.log(data);
       console.info('Action: ' + 'playerLeft', game);
     });
     socket.on('server_message', function(data) {
-      game = data.game;
+      game = data.game || {};
       console.log(data);
+    });
+    socket.on('playerPreMoved', function(data) {
+      game = data.game || {};
+      stateMatrix.text(convertMatrixArrayToPrint(data.stateMatrix));
+      console.log('PreMove from this player group: ', data);
+    });
+    socket.on('playerMoved', function(data) {
+      game = data.game || {};
+      stateMatrix.text(convertMatrixArrayToPrint(data.stateMatrix));
+      console.log('Move from any player: ', data);
     });
     var form = $('#form');
     var game = null;
-    form.submit(function(event) {
+    $('form button').click(function(event) {
+      var $this = $(this);
       event.preventDefault();
-      var action = $('#action').val();
+      var action = $this.attr('id');
       var commands = {
         createGame: function() {
           socket.emit('createGame', {playerName: $('#playerName').val()});
         },
         joinGame: function() {
           socket.emit('joinGame', {playerName: $('#playerName').val(), playerId: $('#gameId').val()});
+        },
+        move: function() {
+          socket.emit('move', {gameId: game.id, playerName: $('#playerName').val(), word: $('#gameId').val()});
+        },
+        preMove: function() {
+          socket.emit('preMove', {gameId: game.id, playerName: $('#playerName').val(), word: $('#gameId').val()});
         }
       };
 
