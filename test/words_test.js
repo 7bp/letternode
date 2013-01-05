0dcdcb3a94bc7d74382aee4e62dcffe60263cbce
knalli <knallisworld@googlemail.com>
2013-01-05 17:58:04 +0100

    Add negative test for words dict.

--- test/words_test.js
+++ test/words_test.js
@@ -5,16 +5,33 @@ var Words = require('../lib/words');
 exports['Words'] = {
   setUp: function(done) {
     done();
+    this.emptyFn = function() {
+    };
   },
   'Load EWOL with Cache': function(test) {
     var words = Words.loadDictionary({
       basePath: 'lib/dictionary/eowl',
       fileNameChars: 1,
       useCache: true
-    });
+    }), emptyFn = this.emptyFn;
     words.onReady(function() {
       words.existWord('alpine').then(function() {
         test.ok(true, 'Word is valid.');
+      }, emptyFn).fin(function() {
+          test.expect(1);
+          test.done();
+        });
+    });
+  },
+  'Load EWOL with Cache (failed)': function(test) {
+    var words = Words.loadDictionary({
+      basePath: 'lib/dictionary/eowl',
+      fileNameChars: 1,
+      useCache: true
+    }), emptyFn = this.emptyFn;
+    words.onReady(function() {
+      words.existWord('willneverexistthisword').then(emptyFn,function() {
+        test.ok(true, 'Word is invalid.');
       }).fin(function() {
           test.expect(1);
           test.done();
@@ -26,10 +43,25 @@ exports['Words'] = {
       basePath: 'lib/dictionary/eowl',
       fileNameChars: 1,
       useCache: false
-    });
+    }), emptyFn = this.emptyFn;
     words.onReady(function() {
       words.existWord('alpine').then(function() {
         test.ok(true, 'Word is valid.');
+      }, emptyFn).fin(function() {
+          test.expect(1);
+          test.done();
+        });
+    });
+  },
+  'Load EWOL without Cache (failed)': function(test) {
+    var words = Words.loadDictionary({
+      basePath: 'lib/dictionary/eowl',
+      fileNameChars: 1,
+      useCache: false
+    }), emptyFn = this.emptyFn;
+    words.onReady(function() {
+      words.existWord('willneverexistthisword').then(emptyFn,function() {
+        test.ok(true, 'Word is invalid.');
       }).fin(function() {
           test.expect(1);
           test.done();
