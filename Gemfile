a197ea0059da27a785eb1b509bbd4d4d9368a5f7
knalli <knallisworld@googlemail.com>
2013-01-11 21:50:36 +0100

    Introduce and add CSS generator SASS w/ library Compass.
    Setup the development environment:
    - *Gem*: adds a Gemfile definition ensuring Ruby dependencies
    - *NPM*: adds `grunt-compass` using to call compass directly by Grunt
    - *Grunt*: defines the compass task (dev/prod) and adds a corresponding watchers (dev only)
    - *Git*: defines in .gitignore that the `.sass-cache` and the actual output `static/css` should be ignored.
    - `*Travis CI*: define the correct SASS/Compass dependencies (see Gemfile!)
    
    Migration of CSS -> SCSS:
    - former `style.css` -> `_style.scss`, `_avgrund_override.scss`
    - former `avgrund.css` -> `_avgrund.scss`
    - compact index file `main.scss` with of three of them
    - use as much Compass mixins as possible
    
    - Change including CSS files in HTML files.

---
+++ Gemfile
@@ -0,0 +1,3 @@
+source "http://rubygems.org"
+gem "sass", "~>3.2.3"
+gem "compass", "~>0.12.2", :require => "sass"
---
+++ compass-config-dev.rb
@@ -0,0 +1,29 @@
+require "rubygems"
+require "bundler/setup"
+
+#For Fire-SASS & CSS Source Maps
+sass_options = { :debug_info => true }
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "static/css"
+sass_dir = "static/scss"
+# Destination of generated images
+images_dir = "static/images"
+sprite_load_path = File.expand_path File.dirname(__FILE__)+"/static/images"
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+line_comments = true
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
